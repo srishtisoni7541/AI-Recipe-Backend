590b7869cff83ecba25ddab2b98258748f2ace3b
srishti soni <srishtisoni7541@gmail.com>
2025-04-03 01:38:48 +0530

    .

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const corsOptions = {
   };
   
   app.use(cors(corsOptions));
+  app.options('*', cors(corsOptions)); 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(helmet());
